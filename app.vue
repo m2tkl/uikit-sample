d6e9c89195f3793ece37d8c63558c42c47f2542a
m2tkl <kenchalros@gmail.com>
2022-02-21 18:26:26 +0900

    initial commit

---
+++ app.vue
@@ -0,0 +1,130 @@
+<script>
+import UIkit from "uikit";
+import Icons from "uikit/dist/js/uikit-icons";
+import "uikit/dist/css/uikit.css";
+import "uikit/dist/css/uikit.min.css";
+UIkit.use(Icons);
+</script>
+
+<template>
+  <header>
+    <nav uk-navbar class="uk-navbar-conatiner uk-background-secondary uk-light">
+      <div class="uk-navbar-left uk-margin-small-left">
+        <a
+          class="uk-navbar-item uk-logo uk-hidden@m"
+          uk-icon="menu"
+          uk-toggle="target: #offcanvas-nav"
+        />
+        <div class="uk-navbar-item uk-logo">
+          <NuxtLink to="/">UIkit</NuxtLink>
+        </div>
+      </div>
+      <div class="uk-navbar-right uk-margin-right">Menu</div>
+    </nav>
+  </header>
+
+  <section class="uk-flex">
+    <div
+      class="
+        sidenav
+        uk-width-medium
+        uk-section
+        uk-section-secondary
+        uk-padding-small
+        uk-visible@m
+      "
+    >
+      <ul uk-nav class="uk-nav-default uk-nav-parent-icon">
+        <li class="uk-nav-header">Container</li>
+        <li>
+          <nuxt-link to="/container/dashboard">Dashboard</nuxt-link>
+        </li>
+        <li class="uk-parent">
+          <a href="#">Parent</a>
+          <ul class="uk-nav-sub">
+            <li>
+              <a href="#">Sub item</a>
+            </li>
+            <li>
+              <a href="#">Sub item</a>
+            </li>
+          </ul>
+        </li>
+        <li class="uk-nav-header">Header</li>
+        <li>
+          <a href="#">
+            <span class="uk-margin-small-right" uk-icon="icon: table"></span>
+            Item</a
+          >
+        </li>
+        <li>
+          <a href="#">
+            <span
+              class="uk-margin-small-right"
+              uk-icon="icon: thumbnails"
+            ></span>
+            Item</a
+          >
+        </li>
+        <li class="uk-nav-divider"></li>
+        <li>
+          <a href="#">
+            <span class="uk-margin-small-right" uk-icon="icon: trash"></span>
+            Item</a
+          >
+        </li>
+      </ul>
+    </div>
+
+    <div
+      class="uk-width-auto"
+      uk-offcanvas="overlay: true; esc-close: true; mode: none;"
+    >
+      <div class="uk-offcanvas-bar">
+        <button class="uk-offcanvas-close" type="button" uk-close></button>
+
+        <div id="offcanvas-nav"></div>
+        <ul class="uk-nav uk-nav-default">
+          <li class="uk-active"><a href="#">Active</a></li>
+          <li class="uk-parent">
+            <a href="#">Parent</a>
+            <ul class="uk-nav-sub">
+              <li><a href="#">Sub item</a></li>
+              <li><a href="#">Sub item</a></li>
+            </ul>
+          </li>
+          <li class="uk-nav-header">Header</li>
+          <li>
+            <a href="#"
+              ><span class="uk-margin-small-right" uk-icon="icon: table"></span>
+              Item</a
+            >
+          </li>
+          <li>
+            <a href="#"
+              ><span
+                class="uk-margin-small-right"
+                uk-icon="icon: thumbnails"
+              ></span>
+              Item</a
+            >
+          </li>
+          <li class="uk-nav-divider"></li>
+          <li>
+            <a href="#"
+              ><span class="uk-margin-small-right" uk-icon="icon: trash"></span>
+              Item</a
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <div
+      class="main uk-width-expand uk-padding"
+      uk-height-viewport="offset-top: 20"
+    >
+      <NuxtPage />
+    </div>
+  </section>
+</template>
---
+++ pages/container/dashboard.vue
@@ -0,0 +1,101 @@
+<template>
+  <div
+    uk-grid
+    class="
+      uk-child-width-1-1@s
+      uk-child-width-1-1@m
+      uk-child-width-1-2@l
+      uk-child-width-1-3@xl
+      uk-grid-small
+      uk-grid-match
+    "
+  >
+    <div>
+      <div class="uk-card uk-card-default uk-card-body">
+        <div uk-grid class="uk-grid-small uk-flex-middle">
+          <div class="uk-width-auto">
+            <span uk-icon="icon: thumbnails; ratio: 2"></span>
+          </div>
+          <div class="uk-width-expand">
+            <h3 class="uk-card-title">Stacks</h3>
+          </div>
+        </div>
+        <p>
+          Lorem ipsum <a href="#">dolor</a> sit amet, consectetur adipiscing
+          elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+          aliqua.
+        </p>
+      </div>
+    </div>
+
+    <div>
+      <div class="uk-card uk-card-default uk-card-body">
+        <div uk-grid class="uk-grid-small uk-flex-middle">
+          <div class="uk-width-auto">
+            <span uk-icon="icon: list; ratio: 2"></span>
+          </div>
+          <div class="uk-width-expand">
+            <h3 class="uk-card-title">Service</h3>
+          </div>
+        </div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+      </div>
+    </div>
+
+    <div>
+      <div class="uk-card uk-card-default uk-card-body">
+        <div uk-grid class="uk-grid-small uk-flex-middle">
+          <div class="uk-width-auto">
+            <span uk-icon="icon: nut; ratio: 2"></span>
+          </div>
+          <div class="uk-width-expand">
+            <h3 class="uk-card-title">Container</h3>
+          </div>
+        </div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+      </div>
+    </div>
+
+    <div>
+      <div class="uk-card uk-card-default uk-card-body">
+        <div uk-grid class="uk-grid-small uk-flex-middle">
+          <div class="uk-width-auto">
+            <span uk-icon="icon: image; ratio: 2"></span>
+          </div>
+          <div class="uk-width-expand">
+            <h3 class="uk-card-title">Images</h3>
+          </div>
+        </div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+      </div>
+    </div>
+
+    <div>
+      <div class="uk-card uk-card-default uk-card-body">
+        <div uk-grid class="uk-grid-small uk-flex-middle">
+          <div class="uk-width-auto">
+            <span uk-icon="icon: database; ratio: 2"></span>
+          </div>
+          <div class="uk-width-expand">
+            <h3 class="uk-card-title">Volumes</h3>
+          </div>
+        </div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+      </div>
+    </div>
+
+    <div>
+      <div class="uk-card uk-card-default uk-card-body">
+        <div uk-grid class="uk-grid-small uk-flex-middle">
+          <div class="uk-width-auto">
+            <span uk-icon="icon: server; ratio: 2"></span>
+          </div>
+          <div class="uk-width-expand">
+            <h3 class="uk-card-title">Networks</h3>
+          </div>
+        </div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+      </div>
+    </div>
+  </div>
+</template>
