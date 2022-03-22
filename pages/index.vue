094a5a8eace78d8d879f01aa1a94536e231173b7
m2tkl <kenchalros@gmail.com>
2022-03-22 20:54:51 +0900

    notify network status

--- pages/index.vue
+++ pages/index.vue
@@ -1,1 +1,23 @@
-<template>Hello</template>
+<script setup lang="ts">
+import UIkit from "uikit";
+
+onMounted(() => {
+  window.addEventListener("offline", function (e) {
+    UIkit.notification({
+      message: "Network: offline",
+      status: "warning",
+      pos: "top-right",
+    });
+  });
+
+  window.addEventListener("online", function (e) {
+    UIkit.notification({
+      message: "Network: online",
+      status: "primary",
+      pos: "top-right",
+    });
+  });
+});
+</script>
+
+<template></template>
